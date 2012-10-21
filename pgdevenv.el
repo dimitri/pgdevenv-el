91b766925ad38788b19d5295e898bd4987d85f50
Dimitri Fontaine <dim@Dimitris-MacBook-Air.local>
2012-10-21 22:59:51 +0200

    Implement the much wanted C-M-a, C-M-e, C-M-h and C-M-x in SQL mode.

--- pgdevenv.el
+++ pgdevenv.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Dimitri Fontaine <dim@tapoueh.org>
 ;; GIT: https://github.com/dimitri/pgdevenv-el
-;; Version: 1.0
+;; Version: 1.1
 ;; Created: 2012-09-28
 ;; Keywords: emacs postgresql development environment shell debug gdb
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
@@ -279,6 +279,293 @@
 
 ;;;###autoload
 (global-set-key (kbd "C-c - n") 'pgdev-open-shell)
+
+;;
+;; Some editing help, in a pgdev-sql minor mode
+;;
+;; C-M-h   mark-pgsql-query
+;; C-M-x   eval-pgsql-query
+;;
+
+;;;###autoload
+(defvar *pgdev-sql-commands*
+  '("ABORT"
+    "ALTER AGGREGATE"
+    "ALTER COLLATION"
+    "ALTER CONVERSION"
+    "ALTER DATABASE"
+    "ALTER DEFAULT PRIVILEGES"
+    "ALTER DOMAIN"
+    "ALTER EVENT TRIGGER"
+    "ALTER EXTENSION"
+    "ALTER FOREIGN DATA WRAPPER"
+    "ALTER FOREIGN TABLE"
+    "ALTER FUNCTION"
+    "ALTER GROUP"
+    "ALTER INDEX"
+    "ALTER LANGUAGE"
+    "ALTER LARGE OBJECT"
+    "ALTER OPERATOR"
+    "ALTER OPERATOR CLASS"
+    "ALTER OPERATOR FAMILY"
+    "ALTER ROLE"
+    "ALTER SCHEMA"
+    "ALTER SEQUENCE"
+    "ALTER SERVER"
+    "ALTER TABLE"
+    "ALTER TABLESPACE"
+    "ALTER TEXT SEARCH CONFIGURATION"
+    "ALTER TEXT SEARCH DICTIONARY"
+    "ALTER TEXT SEARCH PARSER"
+    "ALTER TEXT SEARCH TEMPLATE"
+    "ALTER TRIGGER"
+    "ALTER TYPE"
+    "ALTER USER"
+    "ALTER USER MAPPING"
+    "ALTER VIEW"
+    "ANALYZE"
+    "BEGIN"
+    "CHECKPOINT"
+    "CLOSE"
+    "CLUSTER"
+    "COMMENT"
+    "COMMIT"
+    "COMMIT PREPARED"
+    "COPY"
+    "CREATE AGGREGATE"
+    "CREATE CAST"
+    "CREATE COLLATION"
+    "CREATE CONVERSION"
+    "CREATE DATABASE"
+    "CREATE DOMAIN"
+    "CREATE EVENT TRIGGER"
+    "CREATE EXTENSION"
+    "CREATE FOREIGN DATA WRAPPER"
+    "CREATE FOREIGN TABLE"
+    "CREATE FUNCTION"
+    "CREATE GROUP"
+    "CREATE INDEX"
+    "CREATE LANGUAGE"
+    "CREATE OPERATOR"
+    "CREATE OPERATOR CLASS"
+    "CREATE OPERATOR FAMILY"
+    "CREATE ROLE"
+    "CREATE RULE"
+    "CREATE SCHEMA"
+    "CREATE SEQUENCE"
+    "CREATE SERVER"
+    "CREATE TABLE"
+    "CREATE TABLE AS"
+    "CREATE TABLESPACE"
+    "CREATE TEXT SEARCH CONFIGURATION"
+    "CREATE TEXT SEARCH DICTIONARY"
+    "CREATE TEXT SEARCH PARSER"
+    "CREATE TEXT SEARCH TEMPLATE"
+    "CREATE TRIGGER"
+    "CREATE TYPE"
+    "CREATE USER"
+    "CREATE USER MAPPING"
+    "CREATE VIEW"
+    "DEALLOCATE"
+    "DECLARE"
+    "DELETE"
+    "DISCARD"
+    "DO"
+    "DROP AGGREGATE"
+    "DROP CAST"
+    "DROP COLLATION"
+    "DROP CONVERSION"
+    "DROP DATABASE"
+    "DROP DOMAIN"
+    "DROP EVENT TRIGGER"
+    "DROP EXTENSION"
+    "DROP FOREIGN DATA WRAPPER"
+    "DROP FOREIGN TABLE"
+    "DROP FUNCTION"
+    "DROP GROUP"
+    "DROP INDEX"
+    "DROP LANGUAGE"
+    "DROP OPERATOR"
+    "DROP OPERATOR CLASS"
+    "DROP OPERATOR FAMILY"
+    "DROP OWNED"
+    "DROP ROLE"
+    "DROP RULE"
+    "DROP SCHEMA"
+    "DROP SEQUENCE"
+    "DROP SERVER"
+    "DROP TABLE"
+    "DROP TABLESPACE"
+    "DROP TEXT SEARCH CONFIGURATION"
+    "DROP TEXT SEARCH DICTIONARY"
+    "DROP TEXT SEARCH PARSER"
+    "DROP TEXT SEARCH TEMPLATE"
+    "DROP TRIGGER"
+    "DROP TYPE"
+    "DROP USER"
+    "DROP USER MAPPING"
+    "DROP VIEW"
+    "END"
+    "EXECUTE"
+    "EXPLAIN"
+    "FETCH"
+    "GRANT"
+    "INSERT"
+    "LISTEN"
+    "LOAD"
+    "LOCK"
+    "MOVE"
+    "NOTIFY"
+    "PREPARE"
+    "PREPARE TRANSACTION"
+    "REASSIGN OWNED"
+    "REINDEX"
+    "RELEASE SAVEPOINT"
+    "RESET"
+    "REVOKE"
+    "ROLLBACK"
+    "ROLLBACK PREPARED"
+    "ROLLBACK TO SAVEPOINT"
+    "SAVEPOINT"
+    "SECURITY LABEL"
+    "SELECT"
+    "SELECT INTO"
+    "SET"
+    "SET CONSTRAINTS"
+    "SET ROLE"
+    "SET SESSION AUTHORIZATION"
+    "SET TRANSACTION"
+    "SHOW"
+    "START TRANSACTION"
+    "TABLE"
+    "TRUNCATE"
+    "UNLISTEN"
+    "UPDATE"
+    "VACUUM"
+    "VALUES"
+    "WITH")
+  "List of PostgreSQL SQL commands, used to find start of current query")
+
+;;;###autoload
+(defun pgdev-current-func ()
+  "Return a list of begin and end position of SQL function at point, if any"
+  (save-excursion
+    (let* ((current-pos (point))
+	   (prev-create-function
+	    (progn
+	      (beginning-of-line)
+	      (if (looking-at "create.*function") (point)
+		(re-search-backward "create.*function" nil 'noerror))))
+	   (open-as-$$
+	    (when prev-create-function
+	      ;; limit the search to next semi-colon
+	      (let ((next-semi-col (re-search-forward ";" nil t)))
+		(goto-char prev-create-function)
+		(re-search-forward "AS[^$]*\\$\\([^$\n]*\\)\\$" next-semi-col t))))
+	   ($$-name
+	    (when open-as-$$ (match-string-no-properties 1)))
+	   (close-as-$$
+	    (when open-as-$$
+	      (re-search-forward (format "\\$%s\\$" $$-name) nil t)
+	      (point)))
+	   (terminal-semicolon
+	    (when close-as-$$
+	      (re-search-forward ";" nil t) (point))))
+      ;; return function's boundaries, or nil if point is not in a function
+      (when (and open-as-$$ close-as-$$
+		 (<= prev-create-function current-pos)
+		 (<= current-pos terminal-semicolon))
+	(list prev-create-function terminal-semicolon)))))
+
+;;;###autoload
+(defun pgdev-beginning-of-query ()
+  "Move backward to the beginning of a query."
+  ;; first search of a create function as that could embed about any other
+  ;; SQL command (see PLpgSQL), then look at other cases.
+  (interactive)
+  ;; when already looking-at the beginning of a query, move backward one
+  ;; char before the previous semicolon
+  (when (looking-at (regexp-opt *pgdev-sql-commands*))
+    (re-search-backward (rx (or buffer-start ";"))))
+  ;; now the real search
+  (let ((current-func (pgdev-current-func)))
+    (if current-func
+	(goto-char (first current-func))
+      ;; not editing a function
+      (re-search-backward (rx (or buffer-start ";")))
+      (when (looking-at ";") (forward-char)) ; skip the semicolon itself
+      (while
+	  ;; skip blanks and comments
+	  (or
+	   (while (looking-at (rx (or (any blank space) eol)))
+	     (forward-char))
+	   (while (looking-at (rx "--" (* not-newline) eol))
+	     (forward-line))))
+      (unless (looking-at (regexp-opt *pgdev-sql-commands*))
+	(error "Couldn't find beginning of current SQL query.")))))
+
+;;;###autoload
+(defun pgdev-end-of-query (&optional arg)
+  "Move forward to the end of a top level query."
+  ;; take care of semicolons embedded in a literal
+  (interactive)
+  (let ((current-func (pgdev-current-func)))
+    (if current-func
+	(goto-char (second current-func))
+      ;; not a function, usual simpler rules
+      (pgdev-beginning-of-query)
+      (let* ((begin (point)))
+	(re-search-forward ";" nil t)))))
+
+;;;###autoload
+(defun pgdev-mark-query ()
+  "Put mark at end of the current SQL query and point at beginning"
+  (interactive)
+  (pgdev-beginning-of-query)
+  (push-mark (point))
+  (pgdev-end-of-query)
+  (exchange-point-and-mark))
+
+;;;###autoload
+(defvar *pgdev-eval-last-branch* nil)
+
+;;;###autoload
+(defun pgdev-reset-eval-branch ()
+  "reset where to evaluate the next SQL query"
+  (interactive)
+  (setq *pgdev-eval-last-branch* nil))
+
+;;;###autoload
+(defun pgdev-eval-query (&optional branch)
+  "Evaluate PostgreSQL query at point in target psql buffer"
+  (interactive)
+  (let* ((branch (or *pgdev-eval-last-branch*
+		     (pgdev-read-branch-name)))
+	 (buf    (format "*Shell: PostgreSQL %s*" branch))
+	 (dummy  (pgdev-mark-query))
+	 (query  (buffer-substring-no-properties (region-beginning)
+						 (region-end))))
+    (with-current-buffer buf
+      (setq *pgdev-eval-last-branch* branch)
+      (insert (format "%s" query))
+      (comint-send-input nil t))))
+
+;;;###autoload
+(defvar pgdev-sql-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-M-a")    'pgdev-beginning-of-query)
+    (define-key map (kbd "C-M-e")    'pgdev-end-of-query)
+    (define-key map (kbd "C-M-h")    'pgdev-mark-query)
+    (define-key map (kbd "C-M-x")    'pgdev-eval-query)
+    map)
+  "Keymap for `pgdev-sql-mode'.")
+
+;;;###autoload
+(define-minor-mode pgdev-sql-mode
+  :keymap pgdev-sql-mode-map)
+
+;;;###autoload
+(add-hook 'sql-mode 'pgdev-sql-mode)
 
 (provide 'pgdevenv)
 
